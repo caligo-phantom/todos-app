c9cbceff84765dd9e14174426a2c21f96af2e414
caligo-phantom <siddharthmittal2002@gmail.com>
2022-10-10 21:26:57 +0530

    updated MyComponents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header';
+import Todos from "./MyComponents/Todos";
+import Footer from "./MyComponents/Footer";
 
 function App() {
+  const onDelete = () => {
+    console.log("I am ondelete")
+  }
+  let todos = [
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to the market to get this job done"
+    },
+    {
+      sno: 2,
+      title: "Go to the mall",
+      desc: "You need to go to the mall to get this job done"
+    },
+    {
+      sno: 3,
+      title: "Go to the ghat",
+      desc: "You need to go to the ghat to get this job done"
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="My Todos List" searchBar={false} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
   );
 }
 
