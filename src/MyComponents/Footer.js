29a2c1bd0c31901f2058efd7c0414dc69f1f9c83
caligo-phantom <siddharthmittal2002@gmail.com>
2022-10-11 14:50:08 +0530

    Updated Footer.js

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const Footer = () => {
+  let footerStyle = {
+    position: "absolute",
+    top: "100vh",
+    width: "100%",
+    border: "2px solid red"
+  }
   return (
-    <div>
-      footer works!
-    </div>
+    <footer className='bg-dark text-light py-3' style={footerStyle}>
+      <p className='text-center'>
+        Copyright &copy; Todoslist.com
+      </p>
+    </footer>
   )
 }
 
