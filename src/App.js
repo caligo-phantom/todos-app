a5dbe9301691ce555bcfd28544f6c29f14d73a1e
caligo-phantom <siddharthmittal2002@gmail.com>
2022-10-11 16:19:19 +0530

    adding and deleting todos

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import './App.css';
 import Header from './MyComponents/Header';
 import Todos from "./MyComponents/Todos";
 import Footer from "./MyComponents/Footer";
-import AddTodo from './MyComponents/AddTodo'; 
+import AddTodo from './MyComponents/AddTodo';
 import React, { useState } from 'react';
 
 function App() {
   const onDelete = (todo) => {
     console.log("I am ondelete of todo", todo);
     setTodos(todos.filter((e) => {
-      return e!== todo;
+      return e !== todo;
     }));
   }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc);
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+
   const [todos, setTodos] = useState([
     {
       sno: 1,
@@ -32,7 +52,7 @@ function App() {
   return (
     <>
       <Header title="My Todos List" searchBar={false} />
-      <AddTodo />
+      <AddTodo addTodo={addTodo} />
       <Todos todos={todos} onDelete={onDelete} />
       <Footer />
     </>
