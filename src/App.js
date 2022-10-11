9405cee9bcea3cb5ec9dedc2674fb926953828bf
caligo-phantom <siddharthmittal2002@gmail.com>
2022-10-11 14:22:07 +0530

    State Hook Usage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Header from './MyComponents/Header';
 import Todos from "./MyComponents/Todos";
 import Footer from "./MyComponents/Footer";
+import React, { useState } from 'react';
 
 function App() {
   const onDelete = (todo) => {
     console.log("I am ondelete of todo", todo);
+    setTodos(todos.filter((e) => {
+      return e!== todo;
+    }));
   }
-  let todos = [
+  const [todos, setTodos] = useState([
     {
       sno: 1,
       title: "Go to the market",
@@ -23,7 +27,7 @@ function App() {
       title: "Go to the ghat",
       desc: "You need to go to the ghat to get this job done"
     }
-  ]
+  ]);
   return (
     <>
       <Header title="My Todos List" searchBar={false} />
